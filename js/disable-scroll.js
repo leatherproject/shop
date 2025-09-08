d26ebd710c1fbde0673b9a6e48a8bec8411c8e94
ViacheslavJS <cherniienko7@gmail.com>
2025-09-08 18:20:59 +0300

    minific.

--- js/disable-scroll.js
+++
@@ -1,8 +0,0 @@
-const pageScroll = document.getElementsByTagName('body')[0]; 
-function disablePageScrolling () {
-  pageScroll.style.overflowY = "hidden";  	 
-}
-
-function enablePageScrolling () {
-  pageScroll.style.overflowY = "visible"; // instead of visible - auto or ""
-}
