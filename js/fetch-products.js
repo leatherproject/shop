0aa2a1ff9135c7a9dac7ab31d2ded4dcf7d4abe1
ViacheslavJS <cherniienko7@gmail.com>
2025-09-08 16:40:39 +0300

    update URL

--- js/fetch-products.js
+++ js/fetch-products.js
@@ -17,6 +17,8 @@ async function fetchData() {
   try {
     productsContainer.innerHTML = '<p class="loading">Loading...</p>';
       
+    //const response = await fetch('https://leatherproject.github.io/shop/data/data.json');
+    //const response = await fetch('https://raw.githubusercontent.com/leatherproject/shop/main/data/data.json');
     const response = await fetch('./data/data.json', { cache: 'no-cache' });        
 
     if (!response.ok) throw new Error('500 Internal Server Error');
