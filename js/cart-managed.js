f71aed4f78930c5d68f33bd0d1803aaae65b2d62
ViacheslavJS <cherniienko7@gmail.com>
2025-09-09 10:06:37 +0300

    update 100 html

--- js/cart-managed.js
+++ js/cart-managed.js
@@ -477,13 +477,10 @@ form.addEventListener('submit', (event) => {
 
   // Очистка и сообщение
   localStorage.removeItem('cart');
-  document.getElementById('phoneInput').value = '';
-
-  enablePageScrolling();
-  document.querySelector('.cart__dialog-overlay').classList.remove('c-dialog-view');
-  document.querySelector('.cart__dialog-overlay').classList.add('c-dialog-hide');
-  window.location.reload();
-  //updateInterface();
+  //alert("Спасибо! Заказ отправлен. Мы с вами свяжемся.");
+  //document.getElementById('orderForm').style.display = 'none';
+  //document.getElementById('phoneInput').value = '';
+  updateInterface();
 
 });
 //
