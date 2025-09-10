05d948e4d0d714aa14771e3a1b54bc7a9a116759
ViacheslavJS <cherniienko7@gmail.com>
2025-09-10 11:47:56 +0300

    update

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window.AppConfig = {
   //API_URL: "http://localhost:8000/index.php", // Локальный сервер
-  //API_URL: "https://leatherproject.github.io/shop/data/data.json",
+  API_URL: "https://leatherproject.github.io/shop/data/data.json",
   //API_URL: "https://raw.githubusercontent.com/leatherproject/shop/main/data/data.json",
-  API_URL: "./data/data.json', { cache: 'no-cache' }"
+  //API_URL: "./data/data.json', { cache: 'no-cache' }"
 };
 
 
